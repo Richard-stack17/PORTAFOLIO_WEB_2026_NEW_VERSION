3506de1572026dbbcd143b1c51791e05b2ddfa1d
ralfarom-svg <ralfarom@unac.edu.pe>
2026-05-09 00:46:50 -0500

    fix: update all imports to point to new src/i18n.ts location

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, setRequestLocale } from 'next-intl/server';
 import { notFound } from 'next/navigation';
-import { locales } from '@/i18n/request';
+import { locales } from '@/i18n';
 import { ThemeProvider } from 'next-themes';
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
