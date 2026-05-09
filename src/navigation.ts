d4c9830212c4df76736136e9a2096e9bb5b8bfbe
ralfarom-svg <ralfarom@unac.edu.pe>
2026-05-09 00:48:39 -0500

    fix: update navigation.ts import to new i18n.ts path

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,4 +1,4 @@
 import {createSharedPathnamesNavigation} from 'next-intl/navigation';
-import {locales} from './i18n/request';
+import {locales} from './i18n';
 
 export const {Link, redirect, usePathname, useRouter} = createSharedPathnamesNavigation({locales});
